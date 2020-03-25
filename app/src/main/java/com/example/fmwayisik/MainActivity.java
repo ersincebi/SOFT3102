284a6f943c91af1684ce4535a38da7a602dabc0c
Çağatay Demircan <cagataydmr1@gmail.com>
2020-03-25 22:11:41 +0300

    Pages

--- app/src/main/java/com/example/fmwayisik/MainActivity.java
+++ app/src/main/java/com/example/fmwayisik/MainActivity.java
@@ -2,13 +2,42 @@ package com.example.fmwayisik;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button profilebutton;
+    private Button paymentButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        profilebutton = (Button) findViewById(R.id.profileButton);
+        profilebutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openProfilePage();
+            }
+        });
+        paymentButton = (Button) findViewById(R.id.payButton);
+        paymentButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openPaymentPage();
+            }
+        });
+
+    }
+
+    public void openProfilePage(){
+        Intent intent = new Intent(this,Profile.class);
+        startActivity(intent);
+    }
+    public void openPaymentPage(){
+        Intent intent = new Intent(this,Pay.class);
+        startActivity(intent);
     }
 }
